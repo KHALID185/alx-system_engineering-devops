f8284818789980406601e5402508586e15071fce
KHALID185 <khalid.1998.taouti@gmail.com>
2023-11-24 06:40:29 -0800

    processes and signals

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+
+/**
+* infinite_while - Run an infinite while loop.
+* Return: O success
+*/
+
+int infinite_while(void)
+{
+while (1)
+{
+sleep(1);
+}
+return (0);
+}
+
+/**
+* main - 5 zombie process
+*
+* Return: 0 program run successufly  
+*/
+
+int main(void)
+{
+char counter = 0;
+pid_t PiDD;
+
+while (counter < 5)
+{
+PiDD = fork();
+if (PiDD > 0)
+{
+printf("Zombie process created, PID: %d\n", PiDD);
+sleep(1);
+counter++;
+}
+else
+exit(0);
+}
+infinite_while();
+return (EXIT_SUCCESS);
+}
