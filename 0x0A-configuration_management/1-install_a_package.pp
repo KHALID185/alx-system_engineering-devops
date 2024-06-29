5b8106ae91e08a328104ffaa5f39669998051bd4
KHALID185 <khalid.1998.taouti@gmail.com>
2024-06-28 18:42:11 -0700

    config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,12 @@
 #file to innstall flask
 
 
-exec {'pip3 install flask':
-require => Exec['python-installed'],
-command => '/usr/bin/pip3 install flask==2.1.0'
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  require => Exec['check_python_installed'],
 }
 
-exec {'python-installed':
-command => '/usr/bin/which python3'
+exec { 'check_python_installed':
+  command => '/usr/bin/which python3',
 }
+
