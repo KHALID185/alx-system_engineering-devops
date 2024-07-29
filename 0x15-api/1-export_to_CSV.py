f62f97f94aef285817c506f5fb1569584f41598c
KHALID185 <58513767+KHALID185@users.noreply.github.com>
2024-07-29 12:29:16 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,42 +1,18 @@
 #!/usr/bin/python3
-""" Script to get TODO list progress
-    employee ID and save it to
-    in JSON fomat
-"""
-from json import dump
-from requests import get
-from sys import argv
+"""Exports to-do list information for a given employee ID to CSV format."""
+import csv
+import requests
+import sys
 
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-def todo_json(emp_id):
-    """ Send request for employee's
-        todo list to API
-    """
-    file_name = '{}.json'.format(emp_id)
-    url_user = 'https://jsonplaceholder.typicode.com/users/'
-    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
-
-    # check if user exists
-    user = get(url_user + emp_id).json().get('username')
-
-    if user:
-        params = {'userId': emp_id}
-        #  get all tasks
-        tasks = get(url_todo, params=params).json()
-
-        #  create list of dictionaries
-        #  with info about each task
-        task_list = []
-        for task in tasks:
-            task_list.append({"task": task.get('title'),
-                              "completed": task.get('completed'),
-                              "username": user})
-
-        #  open file in write mode and jsonify
-        with open(file_name, 'w', encoding='utf8') as f:
-            dump({emp_id: task_list}, f)
-
-
-if __name__ == '__main__':
-    if len(argv) > 1:
-        todo_json(argv[1])
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
