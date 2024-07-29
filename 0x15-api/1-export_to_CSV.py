cadf22043c133b4efc7b53b565c7ee21e17c38eb
KHALID185 <khalid.1998.taouti@gmail.com>
2024-07-29 04:33:08 -0700

    api project

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+"""a script that transfert to do list to csv format"""
 import csv
 import requests
 import sys
