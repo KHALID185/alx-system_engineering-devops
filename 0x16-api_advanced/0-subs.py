9f2eca45a412ba416465c9c6998316f00749e61b
KHALID185 <khalid.1998.taouti@gmail.com>
2024-08-14 05:48:09 -0700

    api advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""file that subscriber the reddit"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ a function queries of api reddit num of subs"""
+    base_url = 'https://www.reddit.com/r/'
+
+    url = '{}{}/about.json'.format(base_url, subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    results = requests.get(
+        url,
+        headers=headers,
+        allow_redirects=False
+    }
+    if results.status_code == 200:
+        return results.json()['data']['subscribers']
+    return 0
